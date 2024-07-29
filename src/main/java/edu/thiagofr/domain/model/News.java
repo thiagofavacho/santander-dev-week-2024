07963029a7a10c740959eb7396c4b47614805735
Thiago Favacho R. <thiagofr89@hotmail.com>
2024-07-29 20:54:46 -0300

    second commit

---
+++ src/main/java/edu/thiagofr/domain/model/News.java
@@ -0,0 +1,8 @@
+package edu.thiagofr.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
---
+++ src/main/java/edu/thiagofr/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package edu.thiagofr.domain.repository;
+
+import edu.thiagofr.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
