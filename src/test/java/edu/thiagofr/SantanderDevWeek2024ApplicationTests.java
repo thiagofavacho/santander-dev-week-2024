5a6c33b448164129fe937756647bec8e67123ce1
Thiago Favacho R. <thiagofr89@hotmail.com>
2024-07-29 15:19:15 -0300

    first commit

---
+++ src/test/java/edu/thiagofr/SantanderDevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.thiagofr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
